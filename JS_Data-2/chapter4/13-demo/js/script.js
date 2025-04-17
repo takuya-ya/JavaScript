5ff7de8852b61088bc6d37ef32f5e95cce8389c4
Your Name <your-email@example.com>
2025-04-17 16:23:15 +0900

    chapter4~chapter5の配列まで

--- JS_Data-2/chapter4/13-demo/js/script.js
+++ JS_Data-2/chapter4/13-demo/js/script.js
@@ -1,21 +1,41 @@
+// const getScrollPercent = () => {
+//   // スクロール量
+//   const scrolled = window.scrollY;
+//   console.log(`${scrolled} スクロールされました`);
+
+//   // ページ全体の高さ
+//   const pageHeight = document.documentElement.scrollHeight;
+
+//   // 表示領域の高さ
+//   const viewHeight = document.documentElement.clientHeight;
+//   //console.log(`ページの高さ：${pageHeight}、表示領域の高さ：${viewHeight}`)
+
+//   // スクロールされた割合
+//   const percentage = scrolled / (pageHeight - viewHeight) * 100;
+//   //console.log(percentage);
+  
+//   // プログレスバーに幅を指定
+//   document.querySelector('#bar').style.width = `${percentage}%`;
+// };
+
+// window.addEventListener('scroll', getScrollPercent);
+
+
+
+
 const getScrollPercent = () => {
   // スクロール量
   const scrolled = window.scrollY;
-  //console.log(`${scrolled} スクロールされました`);
-
-  // ページ全体の高さ
+  // 全体の高さ
   const pageHeight = document.documentElement.scrollHeight;
-
-  // 表示領域の高さ
+  // ページの内表示している部分の高さ
   const viewHeight = document.documentElement.clientHeight;
-  //console.log(`ページの高さ：${pageHeight}、表示領域の高さ：${viewHeight}`)
-
-  // スクロールされた割合
-  const percentage = scrolled / (pageHeight - viewHeight) * 100;
-  //console.log(percentage);
   
-  // プログレスバーに幅を指定
-  document.querySelector('#bar').style.width = `${percentage}%`;
-};
+  // スクロール量／（全体の高さーページの高さ）　* 100%
+  const scrollPercent = scrolled / (pageHeight-viewHeight) * 100
+
+
+  document.querySelector('#bar').style.width = `${scrollPercent}%`;
+}
 
 window.addEventListener('scroll', getScrollPercent);
