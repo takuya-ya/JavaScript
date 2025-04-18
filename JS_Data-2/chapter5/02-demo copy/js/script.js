278726178072f5663e1e7064491ffda87ce4af5b
Your Name <your-email@example.com>
2025-04-18 16:46:57 +0900

    feat:アルゴリズムについて、簡単問題に回答

--- JS_Data-2/chapter5/02-demo copy/js/script.js
+++ JS_Data-2/chapter5/02-demo copy/js/script.js
@@ -10,16 +10,36 @@ const lists = [
     'apple.jpg',
 ];
 
-console.log(lists);
-
-const content = 
-`<div><img src="images/${lists[0]}" alt=""></div>
-<div><img src="images/${lists[1]}" alt=""></div>
-<div><img src="images/${lists[2]}" alt=""></div>
-<div><img src="images/${lists[3]}" alt=""></div>
-<div><img src="images/${lists[4]}" alt=""></div>
-<div><img src="images/${lists[5]}" alt=""></div>`
-;
+// const content = 
+// `<div><img src="images/${lists[0]}" alt=""></div>
+// <div><img src="images/${lists[1]}" alt=""></div>
+// <div><img src="images/${lists[2]}" alt=""></div>
+// <div><img src="images/${lists[3]}" alt=""></div>
+// <div><img src="images/${lists[4]}" alt=""></div>
+// <div><img src="images/${lists[5]}" alt=""></div>`
+// ;
 
 // menu.textContent = content;
-menu.insertAdjacentHTML('beforeend', content);
+// menu.insertAdjacentHTML('beforeend', content);
+
+// 画像の増加に対して、自動でiの上限数を更新する為に、要素数を自動取得する方法に変更。
+// for(let i = 0; i < 6; i++) {
+for(let i = 0; i < lists.length; i++) {
+    const content = `<div><img src="images/${lists[i]}" alt=""></div>`;
+    menu.insertAdjacentHTML('beforeend', content);
+}
+
+// コンソールに2の倍数を出力
+const max = 10;
+for(let i = 1; i <= max; i++) {
+    const baisuu = i * 2;
+    console.log(`${baisuu}`);
+}
+
+// 1~30までの整数の偶数だけをコンソールに出力
+
+for (i = 1; i <= 30; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
