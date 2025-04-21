6e9b581aca27a1de3e3f877cb8254f8593b5dec0
Your Name <your-email@example.com>
2025-04-21 13:29:59 +0900

    feat:practice

---
+++ practice/js/script.js
@@ -0,0 +1,16 @@
+
+sum = 0;
+
+for (let i = 1; i <= 10; i++) {
+    sum += i;
+}
+
+console.log(sum);
+
+num = [];
+
+for (let i = 1; i <= 5; i++) {
+    num.push(i * 5);
+}
+
+console.log(num.join(' '));
