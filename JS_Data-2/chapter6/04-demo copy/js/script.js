4f6eb812f340e7f26c8b672f69ebc29bbb7f5978
Your Name <your-email@example.com>
2025-04-22 13:32:05 +0900

    feat:chapter6 アニメーションによる動きを指定

---
+++ JS_Data-2/chapter6/04-demo copy/js/script.js
@@ -0,0 +1,29 @@
+// const heading = document.querySelector('#heading');
+
+// // 変化の内容
+// const keyframes = {
+//   opacity: [0, 1],
+//   // translateの右、下へ移動する値を指定する
+//   translate: ['0 50px', 0],
+// };
+// // 変化の制御
+// const options = {
+//   duration: 2000,
+//   easing: 'ease',
+// };
+
+// heading.animate(keyframes, options);
+
+heading = document.querySelector('#heading');
+
+const keyframes = {
+  opacity : [0, 1],
+  translate : ['0 50px', 0]
+}
+
+const option = {
+  duration : 2000,
+  easing : 'ease'
+}
+
+heading.animate(keyframes, option);
