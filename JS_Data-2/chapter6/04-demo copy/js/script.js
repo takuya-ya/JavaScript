9d77b94ae95a3d9dea665a998bb20c0081c4abe2
Your Name <your-email@example.com>
2025-04-22 14:44:53 +0900

    feat:背景色をスライムのように動かすアニメーションを追加

--- JS_Data-2/chapter6/04-demo copy/js/script.js
+++ JS_Data-2/chapter6/04-demo copy/js/script.js
@@ -20,13 +20,28 @@ const keyframes = {
   // opacity : [0, 1],
   // translate : ['0 50px', 0],
   // rotate : ['x 360deg'],
-  color: ['transparent', '#fff'],
-  backgroundPosition: ['100% 0', '0 0']
+
+  // 見出しタイトル背景色のアニメ
+  // color: ['transparent', '#fff'],
+  // backgroundPosition: ['100% 0', '0 0']
+
+  // スライム的な動き
+    // 上（左）から順に実行される
+  borderRadius : [
+    '20% 50% 50% 70%/50% 50% 70% 50%',
+    '50% 20% 50% 50%/40% 40% 60% 60%',
+    '50% 40% 20% 40%/40% 50% 50% 80%',
+    '50% 50% 50% 20%/40% 40% 60% 60%',
+  ]
 }
 
 const option = {
-  duration : 1000,
-  easing : 'ease'
+  // easing : 'ease'
+  duration : 7000,
+  // 方向：進行方向を往復にする
+  direction: 'alternate',
+  // 反復
+  iterations: Infinity,
 }
 
 heading.animate(keyframes, option);
