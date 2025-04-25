d584346e4fb0ae816ba23eb32db2644dc0e3bc2d
Your Name <your-email@example.com>
2025-04-25 16:39:27 +0900

    chapter7-15までの学習

--- JS_Data-2/chapter7/09-demo/js/script.js
+++ JS_Data-2/chapter7/09-demo/js/script.js
@@ -110,3 +110,34 @@ menuClose.addEventListener('click', () => {
   });
 });
 /* ----- ↑↑ 追加 ↑↑ ----- */
+
+// 監査対象を感知した場合の実行動作
+const animateFade = (entries, obs) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+
+      // console.log(entry.target);
+      entry.target.animate(
+        {
+          opacity: [0, 1],
+          filter: ['blur(.4rem)', 'blur(0)'],
+          translate: ['0 4rem', 0],
+        },
+        {
+          duration: 2000,
+          easing: 'ease',
+          fill: 'forwards'
+        }
+      );
+      obs.unobserve(entry.target);
+    };
+  });
+};
+
+const fadeObserver = new IntersectionObserver(animateFade);
+
+const fadeElements = document.querySelectorAll('.fadein');
+
+fadeElements.forEach((fadeElement) => {
+  fadeObserver.observe(fadeElement);
+});
