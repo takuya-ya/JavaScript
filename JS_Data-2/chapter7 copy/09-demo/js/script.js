4731beeeb78c524a42042e4d8b14656e67a8e320
Your Name <your-email@example.com>
2025-04-25 13:49:44 +0900

    補完機能強化を目的としたjsonファイルを追加し、学習用にディレクトリをコピー。

---
+++ JS_Data-2/chapter7 copy/09-demo/js/script.js
@@ -0,0 +1,128 @@
+/*
+ローディングから画面遷移
+================================================ */
+// @ts-ignore
+const loadingAreaGrey = document.querySelector('#loading');
+// @ts-ignore
+const loadingAreaGreen = document.querySelector('#loading-screen');
+// @ts-ignore
+const loadingText = document.querySelector('#loading p');
+
+window.addEventListener('load', () => {
+  // ローディング中（グレースクリーン）
+  loadingAreaGrey.animate(
+    {
+      opacity: [1, 0],
+      visibility: 'hidden',
+    },
+    {
+      duration: 2000,
+      delay: 1200,
+      easing: 'ease',
+      fill: 'forwards',
+    }
+  );
+
+  // ローディング中（薄緑スクリーン）
+  loadingAreaGreen.animate(
+    {
+      translate: ['0 100vh', '0 0', '0 -100vh']
+    },
+    {
+      duration: 2000,
+      delay: 800,
+      easing: 'ease',
+      fill: 'forwards',
+    }
+  );  
+
+  // ローディング中テキスト
+  loadingText.animate(
+    [
+      {
+        opacity: 1,
+        offset: .8  //80%
+      },
+      {
+        opacity: 0,
+        offset: 1  //100%
+      },
+    ], 
+    {
+      duration: 1200,
+      easing: 'ease',
+      fill: 'forwards',
+    }
+  );
+});
+
+/*
+画像ギャラリー
+================================================ */
+
+// @ts-ignore
+const mainImage = document.querySelector('.gallery-image img');
+// @ts-ignore
+const thumbImages = document.querySelectorAll('.gallery-thumbnails img');
+
+thumbImages.forEach((thumbImage)=>{
+  thumbImage.addEventListener('mouseover', (event) => {
+    // @ts-ignore
+    mainImage.src = event.target.src;
+    mainImage.animate({opacity: [0, 1]}, 500);
+  });
+});
+
+/*
+スライドメニュー
+================================================ */
+/* ----- ↓↓ 追加 ↓↓ ----- */
+// @ts-ignore
+const menuOpen = document.querySelector('#menu-open');
+// @ts-ignore
+const menuClose = document.querySelector('#menu-close');
+// @ts-ignore
+const menuPanel = document.querySelector('#menu-panel');
+// @ts-ignore
+const menuItems = document.querySelectorAll('#menu-panel li');
+// @ts-ignore
+const menuOptions = {
+  duration: 1400,
+  easing: 'ease',
+  fill: 'forwards',
+};
+
+// メニューを開く
+menuOpen.addEventListener('click', () => {
+  // @ts-ignore
+  menuPanel.animate({translate: ['100vw', 0]}, menuOptions);
+
+  // リンクをひとつずつ順に表示
+  menuItems.forEach((menuItem, index) => {
+    //console.log(`${index}番目のリスト`);
+    menuItem.animate(
+      {
+        opacity: [0, 1],
+        // @ts-ignore
+        translate: ['2rem', 0],
+      },
+      {
+        duration: 2400,
+        delay: 300 * index,
+        easing: 'ease',
+        fill: 'forwards',
+      }
+    );
+  });
+});
+
+// メニューを閉じる
+menuClose.addEventListener('click', () => {
+  // @ts-ignore
+  menuPanel.animate({translate: [0, '100vw']}, menuOptions);
+  menuItems.forEach((menuItem) => {
+    // @ts-ignore
+    menuItem.animate({opacity: [1, 0]}, menuOptions);
+  });
+});
+/* ----- ↑↑ 追加 ↑↑ ----- */
