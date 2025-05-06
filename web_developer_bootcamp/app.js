dbaed4afc19749cb4da60426773bfffec5c57e19
takuya <cdwbh564@yahoo.co.jp>
2025-05-06 22:29:08 +0900

    WDB演習:テレビ番組検索

---
+++ web_developer_bootcamp/app.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const form = document.querySelector('#searchForm');
+form.addEventListener('submit', async function(event) {
+  event.preventDefault();
+  const searchTermInput = form.elements.query;
+  const resource = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTermInput.value}`);
+  console.log(resource);
+
+  makeImages(resource.data);
+});
+
+const makeImages = (results) => {
+  for (let result of results) {
+    if (result.show.image) {
+      const img = document.createElement('img');
+    img.src = result.show.image.medium;
+    document.body.append(img);
+
+    }
+  }
+};
