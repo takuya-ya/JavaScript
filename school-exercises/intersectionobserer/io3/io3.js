a778890749cc002337691f1782203f26b77a29f7
Your Name <your-email@example.com>
2025-05-07 15:00:51 +0900

    ref:コメント追加し、可読性向上

---
+++ school-exercises/intersectionobserer/io3/io3.js
@@ -0,0 +1,19 @@
+const ads = document.querySelectorAll('.ad-banner');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            console.log(entry);
+            const adId = entry.target.dataset.adId;
+
+            console.log(`広告 ${adId} が表示されました`);
+            // 一度だけ記録するように監視を解除
+            observer.unobserve(entry.target);
+
+            // サーバー送信などもここで
+            // fetch('/track-ad-view', { method: 'POST', body: JSON.stringify({ adId }) });
+        }
+    });
+}, { threshold: 0.5 }); // 50%見えたらカウント
+
+ads.forEach(ad => observer.observe(ad));
