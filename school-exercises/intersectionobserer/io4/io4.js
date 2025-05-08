1fa19f90b1ab02e07e2c64185f8a8b124c83406d
Your Name <your-email@example.com>
2025-05-08 16:42:46 +0900

    ref:処理の順番を変更し、可読性を向上

--- school-exercises/intersectionobserer/io4/io4.js
+++ school-exercises/intersectionobserer/io4/io4.js
@@ -6,26 +6,24 @@ document.querySelectorAll('.accordion-header').forEach(header => {
     header.addEventListener('click', () => {
         const current = header.parentElement;
 
+        // トグル（三項演算子）
+        current.classList.contains('open')
+        ? current.classList.remove('open')
+        : current.classList.add('open');
+        
         // 他のセクションを閉じる
         accordions.forEach(a => {
             if (a !== current) a.classList.remove('open');
         });
-
-        // トグル（三項演算子）
-        current.classList.contains('open')
-            ? current.classList.remove('open')
-            : current.classList.add('open');
     });
 });
 
-
 // 読了判定（IntersectionObserver）
 const markers = document.querySelectorAll('.read-marker');
-
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
-            // 交差した要素に直近のaccordionクラスを取得して代入
+            // 交差した要素に直近のaccordionクラスだけを取得して代入、いちいち異なるクラスをつけなくていい
             const accordion = entry.target.closest('.accordion');
             // 取得したクラスの要素のIDを取得し、下記で出力する
             const sectionId = accordion.dataset.id;
