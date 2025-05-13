b1c263679fb15bf7426fec85e0a5240f515911be
Your Name <your-email@example.com>
2025-05-13 10:28:55 +0900

    dot:カルーセル

---
+++ dotInstall/MyCarousel/js/main.js
@@ -0,0 +1,25 @@
+'use strict'
+
+{
+  const next = document.getElementById('next');
+  const prev = document.getElementById('prev');
+  const ul = document.querySelector('ul');
+  const slides = ul.children;
+  let currentIndex = 0;
+
+  
+  next.addEventListener('click', () => {
+  currentIndex++;
+  const slideWidth = slides[0].getBoundingClientRect().width;
+  ul.style.transform = `translateX(${-1 * slideWidth * currentIndex}px)`;
+  })
+
+  prev.addEventListener('click', () => {
+  currentIndex--;
+  const slideWidth = slides[0].getBoundingClientRect().width;
+  ul.style.transform = `translateX(${-1 * slideWidth * currentIndex}px)`;
+  })
+
+
+
+}
